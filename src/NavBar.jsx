9867bc8751ab62cef6c0a6d702a4f72be04908a8
LaryeaNii <zadoklaryea@gmail.com>
2023-07-30 19:33:24 +0000

    minor fixes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,37 +8,63 @@ const NavBar = () => {
 
   const navToggle = () => {
     active === 'menu' ? setActive('menu menu-active') : setActive('menu');
-
     toggleIcon === 'hamburger-menu'
       ? setToggleIcon('hamburger-menu toggle')
       : setToggleIcon('hamburger-menu');
   };
 
-  
+  // Function to close the menu when clicking outside
+  const closeMenu = () => {
+    setActive('menu');
+    setToggleIcon('hamburger-menu');
+  };
+
+  // Add event listener on component mount
+  useEffect(() => {
+    document.addEventListener('click', closeMenu);
+    return () => {
+      // Clean up the event listener on component unmount
+      document.removeEventListener('click', closeMenu);
+    };
+  }, []);
+
+  // Prevent the menu from closing when clicking inside the menu
+  const stopPropagation = (event) => {
+    event.stopPropagation();
+  };
 
   return (
     <div>
-    <div className='all-nav'>
-      <nav className="nav-main">
+      <nav className="nav-main" onClick={stopPropagation}>
         <Link to="/">
-           <img src="./CharityLogo.png" alt="the logo" className="logo" />
+          <img src="./CharityLogo.png" alt="the logo" className="logo" />
         </Link>
-        
-        <ul className={active}>
+
+        <ul className={active} onClick={stopPropagation}>
           <li>
-            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/donate" className="nav-link">Donation</Link>
+            <Link to="/donate" className="nav-link">
+              Donation
+            </Link>
           </li>
           <li>
-            <Link to="/team" className="nav-link">Team</Link>
+            <Link to="/team" className="nav-link">
+              Team
+            </Link>
           </li>
           <li>
-            <Link to="/contact" className="nav-link">Contact Us</Link>
+            <Link to="/contact" className="nav-link">
+              Contact Us
+            </Link>
           </li>
         </ul>
         <div onClick={navToggle} className={toggleIcon}>
@@ -49,8 +75,7 @@ const NavBar = () => {
       </nav>
       <hr />
     </div>
-    </div>
   );
-}
+};
 
 export default NavBar;
